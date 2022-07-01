d9e49f9020397d0da73c6b41b0d84ad4c99341c2
Kayne-dev <nnakakenechukwu@gmail.com>
2022-06-30 17:10:20 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("size of char is=%i byte(s)\n", sizeof(char));
+	printf("size of int is=%i byte(s)\n", sizeof(int));
+	printf("size of long int is=%i byte\n", sizeof(long int));
+	printf("size of float is=%i byte\n", sizeof(float));
+	printf("size of short int is=%i byte\n", sizeof(short int));
+	printf("size of long long int is=%i byte\n", sizeof(long long int));
+
+	return (0);
+}
