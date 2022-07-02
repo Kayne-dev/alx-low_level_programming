f5d88d3b539e298d8361acf9b8d255c283f630f3
Kayne-dev <nnakakenechukwu@gmail.com>
2022-07-02 03:41:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints single digit numbers from zero to nine
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+		printf("\n");
+	}
+	return (0);
+}
