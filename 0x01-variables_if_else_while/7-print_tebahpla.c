b4786a772ba1ff3118f02f8a59c89ef072ae0872
Kayne-dev <nnakakenechukwu@gmail.com>
2022-07-02 08:55:23 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+	{
+		putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
+
