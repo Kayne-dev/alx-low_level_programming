87622ce20c2cd8c2d1a560b2d4303ae1fdcc7091
Kayne-dev <nnakakenechukwu@gmail.com>
2022-07-03 03:45:17 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,10 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 90; d++)
+	for (d = 0; d <= 89; d++)
 	{
 		putchar((d / 10) + '0');
 		putchar((d % 10) + '0');
-	}
-	if (d != 89)
-	{
 		putchar(',');
 		putchar(' ');
 	}
